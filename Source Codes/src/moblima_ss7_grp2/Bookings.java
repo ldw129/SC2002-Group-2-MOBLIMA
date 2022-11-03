8126d7975e433ff95167a86c7bff776706902a8f
sgbus <limdongwan201528@gmail.com>
2022-11-03 13:26:10 +0800

    Create Bookings.java

---
+++ Source Codes/src/moblima_ss7_grp2/Bookings.java
@@ -0,0 +1,33 @@
+package moblima_ss7_grp2;
+
+public class Bookings {
+
+	private int bookingID;
+	private int seatNum;
+	private int dateTime;
+
+	public int getBookingID() {
+		return this.bookingID;
+	}
+
+	public void setBookingID(int bookingID) {
+		this.bookingID = bookingID;
+	}
+
+	public int getSeatNum() {
+		return this.seatNum;
+	}
+
+	public void setSeatNum(int seatNum) {
+		this.seatNum = seatNum;
+	}
+
+	public int getDateTime() {
+		return this.dateTime;
+	}
+
+	public void setDateTime(int dateTime) {
+		this.dateTime = dateTime;
+	}
+
+}
