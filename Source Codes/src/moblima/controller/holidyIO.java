cb072887f1852114d7b9918eb25b60ad703702f7
sgbus <limdongwan201528@gmail.com>
2022-11-06 16:56:58 +0800

    FileReader fw changed to fr; removed extra whitespaces

--- Source Codes/src/moblima/controller/holidyIO.java
+++ Source Codes/src/moblima/controller/holidyIO.java
@@ -14,40 +14,36 @@ public class holidyIO {
 
 	    public String[] readHolidays() throws IOException,Exception {
 	    	
-	    		
-	    		FileReader fw = new FileReader("data/Holidays.txt");
-	    		BufferedReader br = new BufferedReader(fw);
-	    		
-	    		try {
-    			return br.readLine().split("[|]");
-	    		}catch(Exception e) {
-	    			String[] temp = new String[0];
-	    			return temp;
-	    		}
-	            	
-	    			
-	    		}	
+			FileReader fr = new FileReader("data/Holidays.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			try {
+				return br.readLine().split("[|]");
+			}
+			catch(Exception e) {
+				String[] temp = new String[0];
+				return temp;
+			}
+		    		
+		}	
 	    
-
 	    /**
 	     * to write a new bookings into the text file
 	     * @throws IOException
 	     * @throws Exception
 	     */
-	    public void writeHoliday(String[] holidays) throws IOException,Exception{
+	    public void writeHoliday(String[] holidays) throws IOException,Exception {
 	    	
 	    	try {
-				
-				
 				FileWriter fw = new FileWriter("data/Holidays.txt", false);
 				BufferedWriter bw = new BufferedWriter(fw);
 				
 				for(int i=0;i<holidays.length;i++)
 					bw.write(holidays[i] + "|");
 
-				bw.close();
-								
-			} catch (IOException e) {
+				bw.close();				
+			} 
+	    	catch (IOException e) {
 				e.printStackTrace();
 			}
 	    	
