b108a449b29d0ab54b13e9ff1daa52165832cd6f
sgbus <limdongwan201528@gmail.com>
2022-11-06 22:27:16 +0800

    Update UpdateHolidays.java

--- Source Codes/src/moblima/controller/UpdateHolidays.java
+++ Source Codes/src/moblima/controller/UpdateHolidays.java
@@ -1,5 +1,33 @@
 package moblima.controller;
 
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class UpdateHolidays {
-    
+	/**
+	 * write the new movie ticket prices into the text file
+	 * @param newPrices the new array of prices
+	 */
+	public void writeHolidays(ArrayList newHolidays) {
+		
+		try {
+		
+		PrintWriter writer = new PrintWriter("DataBase/holidays.txt");
+		
+		for(int i = 0; i < 7; i++) {
+			writer.println(newHolidays.get(i));
+		}
+		
+		writer.print(newHolidays.get(7));
+		
+		writer.close();
+		
+		}
+		
+		catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+		
+	}
 }
