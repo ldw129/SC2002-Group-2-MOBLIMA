3ad1430af117f963b83f78eaa6f2397ffe128686
sgbus <limdongwan201528@gmail.com>
2022-11-09 01:47:56 +0800

    revamped some functions in Movie_goer.java

--- Source Codes/src/moblima/model/Movie_goer.java
+++ Source Codes/src/moblima/model/Movie_goer.java
@@ -56,7 +56,7 @@ public class Movie_goer extends Person {
 		this.password = password;
 	}*/
 
-	public void ListMovie(ArrayList<Movie> movies) {
+	public void ViewMovies(ArrayList<Movie> movies, int numOfMovies) {
 		// Movie_goer can have a glance at a full list of all movies currently showing.
 		// For example:
 		// 1. Black Adam
@@ -64,87 +64,183 @@ public class Movie_goer extends Person {
 		// 3. Black Panther - Wakanda Forever
 		// Movie = ['Black Adam', 'Come Back Home', 'Black Panther - Wakanda Forever']
 		// numOfMovies = 3
+		int i;
+		int movie_index;
+		Movie movie;
 		
-		System.out.println("--- Movie Listing ---");
-		for (int i=0; i< movies.size(); i++)
-			System.out.printf(i+1 + ". " + movies.get(i).getMovieName()+"\n");
+		movie = selectMovie(movies, numOfMovies);
+		ListMovieDetails(movie);
+		Review(movie);
 		
-		System.out.println("Search a movie: ");
-		String movieDesired = sc.next();
+	}
+	
+	public void ListMovieDetails(Movie movie) {
+		int c, j, s;
+		show show;
 		
-		for (int i=0; i< movies.size(); i++) {
-			if (movieDesired == movies.get(i).getMovieName()) {
-				System.out.println(movies.get(i).getMovieName() + " movie found!");
-				return;
+		System.out.println("Movie Title: " + movie.getMovieName());
+		System.out.println("Directed By: " + movie.getDirectorName());
+		
+		System.out.println("Cast: ");
+		String [] cast = movie.getCast();
+		for (c=0; c<cast.length; c++) {
+			System.out.printf("%s \n", cast[c]);
+			if (cast[c].equals("null"))
+				continue;
+		}
+		System.out.println("Showing Status: " + movie.getShowingStatus());
+		
+		System.out.println("Movie Synopsis: ");
+		System.out.println(movie.getSynopsis());
+		
+		System.out.println("Review(s) of the movie: ");
+		String [] reviews = movie.getReviews();
+		if (reviews.length != 0) {
+			for (j=0; j<reviews.length; j++) {
+				System.out.printf("%s \n", reviews[j]);
 			}
 		}
+		else
+			System.out.println("No reviews available.");
 		
-		System.out.println("Movie not found! Do you want to find another movie instead?");
-	}		
+		System.out.println("Movie Rating(s): ");
+		double [] ratings = movie.getAllRatings();
+		if (ratings.length != 0) {
+			for(j=0; j<ratings.length; j++) {
+				System.out.printf("%s \n", ratings[j]);
+			}
+		}
+		else
+			System.out.println("No ratings available.");
+		
+		System.out.println(" ");
+		System.out.println("-------------------------");
+		System.out.println(" ");
+		System.out.println("--- Shows available for this movie --- ");
+		
+		ArrayList<show> showsForSelectedMovie = movie.getShows();
+		
+		for (s=0; s<showsForSelectedMovie.size(); s++) {
+			show = showsForSelectedMovie.get(s);
+			System.out.printf("Show %d\n", s+1);
+			System.out.println("Showtimes: ");
+			System.out.println(show.getDateTime());
+			System.out.printf("Cineplex: %d\n", show.getCineplexID()+1);
+			System.out.printf("Theatre: %d\n", show.getScreenNum()+1);
+			System.out.println("\n-------------------------");
+		}
+	}
+	
+	public void Review(Movie movie) {
+		// Movie_goer can enter his / her review and rating for a particular movie.
+		int choice;
+		
+		System.out.println("Would you like to:\n"
+				+ "1. Review this movie?\n"
+				+ "2. Rate this movie?\n"
+				+ "3. Skip for now.\n");
+		
+		choice = sc.nextInt();
+		do {
+			switch(choice) {
+				case 1:
+					System.out.println("Write your review for this movie.");
+					String review = sc.nextLine();
+					movie.writeReview(review);
+					System.out.println("New review added to the movie!");
+					break;
+				case 2:
+					System.out.println("Rate this movie, from 0 to 5.");
+					double rating = sc.nextDouble();
+					movie.giveRating(rating);
+					System.out.println("New rating added to the movie!");
+					break;
+			}
+		} while(choice != 3);
+		return;
+	}
 
 	public void CheckSeat(ArrayList<show> shows) {
 		// Movie_goer can check for empty seats in a cineplex before booking.
+		master m = new master();
+		int cinemaChoice;
+		Cineplex cineplexChoice;
 		
 		System.out.println("--- Seat Availability ---");
 		
-		for (show show:shows) {
-			show.printSeats();
+		ArrayList<Cineplex> Cineplexes = new ArrayList<Cineplex>();
+		Cineplexes = m.getCineplexes();
+		
+		cineplexChoice = selectCineplex(Cineplexes, Cineplexes.size());
+		
+		System.out.println("Select theatre - 1, 2 or 3:");
+		cinemaChoice = sc.nextInt();
+		while (cinemaChoice > 3) {
+			System.out.println("Invalid input!");
+			sc.nextLine();
 		}
+		
+		System.out.println("--- All available movies at selected theatre ---");
+		ArrayList<show> movieList = cineplexChoice.getCinema().get(cinemaChoice - 1).getCinemaShows();
+		
+		for (show show:shows)
+			show.printSeats();
 	}
 
-	public int selectMovie() {
+	public Movie selectMovie(ArrayList<Movie> movies, int numOfMovies) {
 		// List out all available movies that are Now Showing or for Preview. -> Movie.getStatus()
 		// ListMovie();
 		// User chooses the movie he / she wants to watch at the cinema.
-		int movieChoice = sc.nextInt();
-		return movieChoice;
+		int i;
+		int movie_index = 0;
+		Movie movie;
+		
+		do {
+			System.out.println("Select a movie below: ");
+			System.out.println("--- Movie Listing ---");
+			for (i=0; i<numOfMovies; i++)
+				System.out.printf(i+1 + ". " + movies.get(i).getMovieName() + "\n");
+			System.out.println((i+1) + ". Back");
+			
+			try {
+				movie_index = sc.nextInt();
+				
+			}
+			catch (Exception e) {
+                System.err.println("Invalid input!");
+                sc.nextLine();
+			}
+			
+			movie = movies.get(movie_index-1);
+			
+		} while (movie_index != (i+1));
+		return movie;
 	}
 	
-	public int selectCineplex(int movieChoice) {
+	public Cineplex selectCineplex(ArrayList<Cineplex> Cineplexes, int numOfCineplexes) {
 		int i;
 		int cineplexChoice = 0;
-		int numOfCineplexes = 0;
-		Cineplex cineplex = new Cineplex("null", 0, null);
+		Cineplex cineplex;
 		
-		switch (movieChoice) {
-			case 1:
-				// List out all cineplexes that screens the user's desired (chosen) movie.
-				for (i=0; i<numOfCineplexes; i++) {
-					System.out.println(i+1 + ". " + cineplex.showLocation());
-				}
+		do {
+			System.out.println("Select Cineplex: ");
+			System.out.println("--- Cineplex Listing ---");
+			for (i=0; i<numOfCineplexes; i++)
+				System.out.printf(i+1 + ". " + Cineplexes.get(i).showLocation() + "\n");
+			System.out.println((i+1) + ". Back");
+			
+			try {
 				cineplexChoice = sc.nextInt();
-				break;
-			case 2:
-				// List out all cineplexes that screens the user's desired (chosen) movie.
-				for (i=0; i<numOfCineplexes; i++) {
-					System.out.println(i+1 + ". " + cineplex.showLocation());
-				}
-				cineplexChoice = sc.nextInt();
-				break;
-			case 3:
-				// List out all cineplexes that screens the user's desired (chosen) movie.
-				for (i=0; i<numOfCineplexes; i++) {
-					System.out.println(i+1 + ". " + cineplex.showLocation());
-				}
-				cineplexChoice = sc.nextInt();
-				break;
-			case 4:
-				// List out all cineplexes that screens the user's desired (chosen) movie.
-				for (i=0; i<numOfCineplexes; i++) {
-					System.out.println(i+1 + ". " + cineplex.showLocation());
-				}
-				cineplexChoice = sc.nextInt();
-				break;
-			case 5:
-				// List out all cineplexes that screens the user's desired (chosen) movie.
-				for (i=0; i<numOfCineplexes; i++) {
-					System.out.println(i+1 + ". " + cineplex.showLocation());
-				}
-				cineplexChoice = sc.nextInt();
-				break;
-		}
-		
-		return cineplexChoice;
+			}
+			catch (Exception e) {
+                System.err.println("Invalid input!");
+                sc.nextLine();
+			}
+			
+			cineplex = Cineplexes.get(cineplexChoice-1);
+			
+		} while(cineplexChoice != (i+1));
+		return cineplex;
 	}
 	
 	public int selectDate() {
@@ -211,7 +307,7 @@ public class Movie_goer extends Person {
 		System.out.println("Select a timeslot: ");
 		timeChoice = selectTime(movieChoice, cineplexChoice, dateChoice);
 		
-		// b.getBookingID();
+		b.getBookingID();
 		b.getSeatNum();
 		b.getDateTime();
 	}
@@ -223,20 +319,8 @@ public class Movie_goer extends Person {
 		System.out.println("Here are the top 5 rated movies: ");
 		
 		for (int i=0; i<5; i++) {
-			// System.out.println(i+1 + ". " + popularMovie[i]);
+			System.out.println(i+1 + ". " + popularMovie[i]);
 		}
-	}
-
-	public void Review() {
-		// Movie_goer can enter his / her review and rating for a particular movie.
-		System.out.println("Rate this movie: ");
-		int movieRating = sc.nextInt();
-		
-		System.out.println("Write your review for this movie.");
-		String review = sc.next();
-		
-		System.out.println();
-		
 	}
 	
 	public ArrayList<Bookings> getBookingHistory(){
