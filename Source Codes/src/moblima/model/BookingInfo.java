d0ebf334c5ded75c65a891450abfa850a504e8d5
sgbus <limdongwan201528@gmail.com>
2022-11-10 16:41:09 +0800

    updated and fixed

--- Source Codes/src/moblima/model/BookingInfo.java
+++ Source Codes/src/moblima/model/BookingInfo.java
@@ -2,7 +2,7 @@ package moblima.model;
 
 public class BookingInfo extends Movie_goer {
 
-	private int customerName;
+	private String customerName;
 	String transactionID;
 	private String emailAdd;
 	private String movie;
@@ -12,7 +12,7 @@ public class BookingInfo extends Movie_goer {
 	
 	public BookingInfo() {}
 	
-	public BookingInfo(int cust_name, String transaction_id, String email_id, String movieBooked, int numOfSeats, String firstSeat) {
+	public BookingInfo(String cust_name, String transaction_id, String email_id, String movieBooked, int numOfSeats, String firstSeat) {
 		this.customerName = cust_name;
 		this.transactionID = transaction_id;
 		this.emailAdd = email_id;
@@ -21,13 +21,37 @@ public class BookingInfo extends Movie_goer {
 		// Assume the user books neighbouring seats, to the left or right of firstSeat.
 		this.firstSeat = firstSeat;
 	}
+	
+	public String getCustName() {
+		return this.customerName;
+	}
+	
+	public void setCustName(String cust_name) {
+		this.customerName = cust_name;
+	}
 
 	public String getTID() {
 		return this.transactionID;
 	}
 
-	public void setTID(String transactionID) {
-		this.transactionID = transactionID;
+	public void setTID(String transaction_id) {
+		this.transactionID = transaction_id;
+	}
+	
+	public String getEmailAddress() {
+		return this.emailAdd;
+	}
+	
+	public void setEmailAddress(String email_id) {
+		this.emailAdd = email_id;
+	}
+	
+	public String getMovieBooked() {
+		return this.movie;
+	}
+	
+	public void setMovieBooked(String movieBooked) {
+		this.movie = movieBooked;
 	}
 
 	public int getSeatNum() {
@@ -36,6 +60,14 @@ public class BookingInfo extends Movie_goer {
 
 	public void setSeatNum(int seatNum) {
 		this.seatNum = seatNum;
+	}
+	
+	public String getFirstSeat() {
+		return this.firstSeat;
+	}
+	
+	public void setFirstSeat(String firstSeat) {
+		this.firstSeat = firstSeat;
 	}
 
 	public String getDateTime() {
