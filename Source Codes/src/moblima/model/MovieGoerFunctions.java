32c01ef9bb7e1117dcdf5eb86523aaeea3785faa
sgbus <limdongwan201528@gmail.com>
2022-11-12 02:18:16 +0800

    updated BookTickets, viewBookingHistory, enum Age and validateAge

--- Source Codes/src/moblima/model/MovieGoerFunctions.java
+++ Source Codes/src/moblima/model/MovieGoerFunctions.java
@@ -2,6 +2,8 @@ package moblima.model;
 
 import java.util.*;
 import java.time.format.DateTimeFormatter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -11,10 +13,32 @@ import moblima.model.Movie;
 import moblima.model.master;
 import moblima.model.show;
 import moblima.controller.Movie_goer_IO;
+import moblima.controller.HolidayConfig;
 
 public class MovieGoerFunctions {
 	
 	Scanner sc = new Scanner(System.in);
+	Movie_goer_IO mg = new Movie_goer_IO();
+	
+	public enum AgeCat{
+		CHILD(0, 12),
+		STUDENT(13, 18),
+		ADULT(19, 54),
+		SENIOR_CITIZEN(55, 100);
+		
+		private final int low;
+		private final int high;
+		private final static Set<Integer> ages = new HashSet<Integer>(AgeCat.values().length);
+		
+		private AgeCat(int low, int high) {
+			this.low = low;
+			this.high = high;
+		}
+		
+		public static boolean contains (int ageVal) {
+			return ages.contains(ageVal);
+		}
+	}
 
     public MovieGoerFunctions() {}
 
@@ -160,7 +184,6 @@ public class MovieGoerFunctions {
             System.out.println("--- Movie Listing ---");
             for (i = 0; i < numOfMovies; i++)
                 System.out.printf(i + 1 + ". " + movies.get(i).getMovieName() + "\n");
-            System.out.println((i + 1) + ". Back");
 
             try {
                 movie_index = sc.nextInt();
@@ -175,7 +198,7 @@ public class MovieGoerFunctions {
                 break;
             }
 
-        } while (movie_index != (i + 1));
+        } while (movie_index > numOfMovies);
         
         System.out.println(movie.getMovieName());
         return movie;
@@ -191,7 +214,6 @@ public class MovieGoerFunctions {
             System.out.println("--- Cineplex Listing ---");
             for (i = 0; i < numOfCineplexes; i++)
                 System.out.printf(i + 1 + ". " + Cineplexes.get(i).showLocation() + "\n");
-            System.out.println((i + 1) + ". Back");
 
             try {
                 cineplexChoice = sc.nextInt();
@@ -202,7 +224,9 @@ public class MovieGoerFunctions {
 
             cineplex = Cineplexes.get(cineplexChoice - 1);
 
-        } while (cineplexChoice != (i + 1));
+        } while (cineplexChoice > numOfCineplexes);
+        
+        System.out.println(cineplex.showLocation());
         return cineplex;
     }
 
@@ -222,6 +246,24 @@ public class MovieGoerFunctions {
 
         dateChoice = sc.nextInt();
         return dateChoice;
+    }
+
+    public int selectTime(int movieChoice, int cineplexChoice, int dateChoice) {
+        // List out all the available time slots of when the user's desired (chosen)
+        // movie will be screened on a chosen date.
+        // Cinema.getShowtime();
+        int timeChoice;
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime currTime = LocalTime.now();
+        String time = currTime.format(timeFormatter);
+        LocalTime parsedCurrTime = LocalTime.parse(time, timeFormatter);
+
+        // get movie showtimes on the chosen day (use switch case statement, based on
+        // dateChoice)
+        // list out movie showtimes that are available for booking, if they are beyond
+        // parsedCurrTime, otherwise do not show
+        timeChoice = sc.nextInt();
+        return timeChoice;
     }
     
     public void PopularMovies(ArrayList<Movie> movieList) {
@@ -245,7 +287,7 @@ public class MovieGoerFunctions {
                     case 1:
                         Collections.sort(movieList, Movie.topratings);
                         System.out.println(
-                                "Top 5 rated movies based on overall reviewers' ratings, with first movie having highest rating: ");
+                        "Top 5 rated movies based on overall reviewers' ratings, with first movie having highest rating: ");
                         for (i = movieList.size() - 1; i >= 0; i--)
                             System.out.println(
                                     movieList.get(i).getMovieName() + ": " + movieList.get(i).getTotalRating());
@@ -253,7 +295,7 @@ public class MovieGoerFunctions {
                     case 2:
                         Collections.sort(movieList, Movie.topticketsales);
                         System.out.println(
-                                "Top 5 rated movies based on overall ticket sales, with first movie having highest ticket sales: ");
+                        "Top 5 rated movies based on overall ticket sales, with first movie having highest ticket sales: ");
                         for (i = movieList.size() - 1; i >= 0; i--)
                             System.out.println(movieList.get(i).getMovieName() + ": " + movieList.get(i).getTsales());
                         break;
@@ -268,25 +310,7 @@ public class MovieGoerFunctions {
         return;
     }
 
-    public int selectTime(int movieChoice, int cineplexChoice, int dateChoice) {
-        // List out all the available time slots of when the user's desired (chosen)
-        // movie will be screened on a chosen date.
-        // Cinema.getShowtime();
-        int timeChoice;
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime currTime = LocalTime.now();
-        String time = currTime.format(timeFormatter);
-        LocalTime parsedCurrTime = LocalTime.parse(time, timeFormatter);
-
-        // get movie showtimes on the chosen day (use switch case statement, based on
-        // dateChoice)
-        // list out movie showtimes that are available for booking, if they are beyond
-        // parsedCurrTime, otherwise do not show
-        timeChoice = sc.nextInt();
-        return timeChoice;
-    }
-
-    public void BookTickets() {
+    public void BookTickets() throws FileNotFoundException {
         // Movie_goer can book and purchase movie ticket(s) for a particular chosen
         // movie.
         BookingInfo booking = new BookingInfo();
@@ -294,47 +318,179 @@ public class MovieGoerFunctions {
         int cineplexChoice;
         int dateChoice;
         int timeChoice;
+        
+        master Master = new master();
+        ArrayList<Movie> moviesAvailableForBooking = new ArrayList<Movie>();
+        moviesAvailableForBooking = Master.getMovies();
+        Movie m;
+        show show;
+        Movie_goer_IO mg = new Movie_goer_IO();
+        Movie_goer user = new Movie_goer();
+        
+        int cust_id = 0;
+        int movie_ID = 0;
+        int selection;
+        String movieBooked, temp, firstSeat, cust_name, cust_email, ageCat, bookingConfirmation, cancel, transaction_id;
+        int numSeats, cust_mobile, cust_age, show_index;
+        boolean validAge;
+        
+        String[] holidayList = null;
+        boolean publicHols = false;
+        HolidayConfig holIO = new HolidayConfig();
+        holidayList = holIO.readHolidays();  
+        String cinemaClass;
 
         System.out.println("--- Ticket Booking & Purchase ---");
+        
+        try {
+        	System.out.println("Enter your customer ID to proceed: ");
+        	cust_id = sc.nextInt();
+        } catch (Exception e) {
+            System.err.println("Invalid input!");
+            sc.nextLine();
+        }
+        
+        System.out.println("Enter your name: ");
+        cust_name = sc.next();
+        
+        System.out.println("Enter your mobile number: ");
+        cust_mobile = sc.nextInt();
+        
+        System.out.println("Enter your email address: ");
+        cust_email = sc.next();
+        
+        do {
+        	ageCat = null;
+        	selection = 0;
+        	System.out.println("Choose your age category: "
+            		+ "1. Senior Citizen \n"
+            		+ "2. Adult \n"
+            		+ "3. Student \n"
+            		+ "4. Child \n\n"
+            		+ "Note: \n"
+            		+ "- Senior Citizen: Aged 55 and above  \n"
+            		+ "- Student: Between 13 and 18 years old \n"
+            		+ "- Child: Below 12 years old");
+        	
+        	try {
+        		// to be referenced with enum Age in Movie_goer.java
+        		selection = sc.nextInt();
+        		switch (selection) {
+        			case 1:
+        				System.out.println("Validation of your age and identity will be carried out physically at the cineplex.");
+        				ageCat = "Senior Citizen";
+        				break;
+        			case 2:
+        				System.out.println("Enter your age for validation of your eligibility for an ADULT ticket: ");
+        				cust_age = sc.nextInt();
+        				validateAge(cust_age);
+        				ageCat = "Adult";
+        				break;
+        			case 3:
+        				System.out.println("Enter your age for validation of your eligibility for a STUDENT ticket: ");
+        				cust_age = sc.nextInt();
+        				validateAge(cust_age);
+        				ageCat = "Student";
+        				break;
+        			case 4:
+        				System.out.println("Enter your age for validation of your eligibility for a CHILD ticket: ");
+        				cust_age = sc.nextInt();
+        				validateAge(cust_age);
+        				ageCat = "Child";
+        				break;
+        		}
+        	} catch (Exception e) {
+                System.err.println("Invalid input!");
+                sc.nextLine();
+        	}
+        } while (selection > 4);
+        System.out.println("Invalid input!");
+        
+        m = selectMovie(moviesAvailableForBooking, moviesAvailableForBooking.size());        
 
-        ArrayList<Movie> movies = new ArrayList<Movie>();
-        int custID = 0;
-        int movieID = 0;
-        String movieBooked, temp, firstSeat;
-        int numSeats;
-        Movie m;
-        master Master = new master();
-        movies = Master.getMovies();
-
-        System.out.println("Select a movie:");
-
-        System.out.println("Select a cineplex: ");
-
-        System.out.println("Select a date: ");
-        dateChoice = selectDate();
-
+        ArrayList<show> tempShows = m.getShows();
+        
+        for (int i = 0; i < tempShows.size(); i++) {
+        	publicHols = false;
+        	show = tempShows.get(i);
+        	temp = show.getDateTime().split("")[0];
+        	
+        	for (int h = 0; h < holidayList.length; h++) {
+        		if (holidayList[h].equals(temp)) {
+        			publicHols = true;
+        			break;
+        		}
+        	}
+        	
+			cinemaClass = Master.getCineplexes().get(show.getCineplexID()).getCinema().get(show.getScreenNum()).getCinemaClass();
+			// MovieTicket price = new MovieTicket(show.get3D(), movieDetails, ageCat, publicHols);
+			// MovieTicket price = new MovieTicket(String typeofmovie, String cinemaclass, int age, int date);
+			System.out.printf("\n\nShow %d:\n", i+1);
+			System.out.println("Date Time: " + show.getDateTime());
+			System.out.printf("Cineplex ID: %d\n", show.getCineplexID()+1);
+			System.out.printf("Cinema ID: %d\n", show.getScreenNum()+1);
+			System.out.printf("Cinema Class: %s\n", cinemaClass);
+			System.out.printf("3D: %s\n", show.get3D());
+			// System.out.printf("Ticket Price: S$%s (Inclusive of GST)\n", price.getPrice());
+			System.out.println("----------------");
+        }
+        
+        System.out.println("Enter show index: ");
+        show_index = sc.nextInt() - 1;
+        
+        show = tempShows.get(show_index);
+        show.printSeats();
+        
+        System.out.println("Enter the total number of seats: ");
+        numSeats = sc.nextInt();
+        
+        System.out.println("Enter the first seat: "); // seat ID formatting to be confirmed
+        firstSeat = sc.nextLine();
+        
+        System.out.println("Confirm booking? Y/N");
+        bookingConfirmation = sc.nextLine();
+        if (bookingConfirmation == "N") {
+        	System.out.println("Confirm cancellation? Y/N");
+        	cancel = sc.nextLine();
+        	if (cancel == "Y") {
+        		System.out.println("Booking cancelled!");
+        	}
+        }
+        else {
+        	System.out.println("Booking ID: " + transaction_id);
+        	System.out.println("Movie tickets successfully booked!");
+        }
+        
         System.out.println("Select a timeslot: ");
         timeChoice = selectTime(movieChoice, cineplexChoice, dateChoice);
 
         booking.getTID();
         booking.getSeatNum();
         booking.getDateTime();
+        
+        return;
     }
     
-    public void viewBookingHistory(){
+    public void viewBookingHistory() throws IOException {
 		// Movie_goer can browse through his / her past movie bookings with ease.
-		int cust_id, i;
+		int cust_id = 0;
+		int i;
 		ArrayList<BookingInfo> bookings = new ArrayList<>();
 		Movie_goer customer = new Movie_goer();
-		Movie_goer_IO mg = new Movie_goer_IO();
-		
-    	System.out.println("--- Your Past Bookings ---");
-    	System.out.println("Enter your customer ID to proceed: ");
-    	cust_id = sc.nextInt();
+    	
+		try {
+        	System.out.println("Enter your customer ID to proceed: ");
+        	cust_id = sc.nextInt();
+        } catch (Exception e) {
+            System.err.println("Invalid input!");
+            sc.nextLine();
+        }
     	
     	customer = mg.getMovieGoer(cust_id);
     	
     	bookings = customer.getBooking();
+    	
+    	System.out.println("--- Your Past Bookings ---");
     	
     	for (i = 0; i < bookings.size(); i++) {
     		System.out.println("BOOKING " + (i+1) + ": ");
@@ -344,10 +500,21 @@ public class MovieGoerFunctions {
     		System.out.println("Customer Email ID: " + bookings.get(i).getEmailAddress());
 			System.out.println("Movie Booked: " + bookings.get(i).getMovieBooked());
 			System.out.println("Date & Time: " + bookings.get(i).getDateTime());
-			System.out.println("Number of Seats booked: " + bookings.get(i).getSeatNum());
-			System.out.println("FirstSeat booked: " + bookings.get(i).getFirstSeat());
+			System.out.println("Total number of seats booked: " + bookings.get(i).getSeatNum());
+			System.out.println("First seat booked: " + bookings.get(i).getFirstSeat());
     	}
- 
+	}
+    
+    public void validateAge(int ageVal) {
+		// Movie_goer needs to validate their age when purchasing tickets online, except if he / she is a senior citizen then
+		// he / she will only validate his / her age upon entering the cinema.
+		boolean validAge;
+    	
+    	validAge = AgeCat.contains(ageVal);
+    	if (validAge == true)
+			System.out.println("You are eligible to purchase tickets of this age category!");
+		else
+			System.out.println("You are NOT eligible to purchase tickets of this age category!");
 	}
 
 }
