ca070278b60374bfad96d17a844d9ae38e8b2d5d
ongkangrui <ongkangrui@gmail.com>
2022-11-12 16:24:31 +0800

    movieticketprice done

--- Source Codes/src/moblima/model/MovieTicket.java
+++ Source Codes/src/moblima/model/MovieTicket.java
@@ -2,6 +2,7 @@ package moblima.model;
 
 import java.time.*;
 import moblima.controller.GetPrice;
+import moblima.controller.HolidayConfig;
 
 public class MovieTicket {
     /**
@@ -10,25 +11,25 @@ public class MovieTicket {
     private boolean is3D;
 
     /**
-     * 
+     * Class of cinema
      */
     private String cinemaClass;
 
     /**
-     * 
+     * Age of movie-goer
      */
     private int age;
 
     /**
-     * 
+     * Date of movie
      */
-    private int date;
+    private String date;
 
     /**
-     * 
+     * Price of movie ticket
      */
     private double price;
-    GetPrice getPrice = new GetPrice();
+    GetPrice categoryPrice = new GetPrice();
 
     /**
      * Constructor
@@ -37,7 +38,7 @@ public class MovieTicket {
      * @param age
      * @param date
      */
-    public MovieTicket(boolean is3D, String cinemaClass, int age, int date) {
+    public MovieTicket(boolean is3D, String cinemaClass, int age, String date) {
         this.is3D = is3D;
         this.cinemaClass = cinemaClass;
         this.age = age;
@@ -50,21 +51,41 @@ public class MovieTicket {
      * @return Price of Movie Ticket
      */
     private double calculatePrice() {
+        master m = new master();
+        m.readHolidays();
+        
         double price = 0;
+        String[] holiday_list;
+        holiday_list = m.getHolidays();
 
         switch(cinemaClass) {
             case "Standard":
-                price = getPrice.getStandardPrice();
+                price = categoryPrice.getStandardPrice();
                 break;
             case "Premium":
-                price = getPrice.getPremiumPrice();
+                price = categoryPrice.getPremiumPrice();
                 break;
             case "Plainum":
-                price = getPrice.getPlatinumPrice();
+                price = categoryPrice.getPlatinumPrice();
                 break;
         }
 
-        price += is3D ? getPrice.get3DPrice() : 0;
+        price += is3D ? categoryPrice.get3DPrice() : 0;
+
+        for(String i: holiday_list) {
+            if (i.equals(date)) price += categoryPrice.getPublicHolidayPrice();
+        }
+
+        if(age >= 0 && age < 13 ) {
+            price -= categoryPrice.getChildPrice();
+        }
+        else if (age >= 19 && age < 55) {
+            price -= categoryPrice.getStudentPrice();
+        }
+        else if (age >= 55 && age < 101) {
+            price -= categoryPrice.getSeniorCitizenPrice();
+        }
+
         return price;
     }
 
--- Source Codes/src/moblima/model/master.java
+++ Source Codes/src/moblima/model/master.java
@@ -8,23 +8,29 @@ import java.io.IOException;
 
 /**
  * Master class to serve as the primary backend layer for the UI
+ * 
  * @version 1.0
  */
 
 public class master { // HOLIDAY IS COMMENTED OUT FOR TESTING
 
-	//static private ArrayList<Holiday> holidayList;
-	
+	// static private ArrayList<Holiday> holidayList;
+
 	/**
 	 * Array of all movies
 	 */
 	static private ArrayList<Movie> movieList;
-	
+
 	/**
 	 * Array of all Cineplexes
 	 */
 	private ArrayList<Cineplex> cineplexList;
-	
+
+	/**
+	 * Array of all holidays
+	 */
+	private String[] holiday_list;
+
 	/**
 	 * file for all movies
 	 */
@@ -32,89 +38,93 @@ public class master { // HOLIDAY IS COMMENTED OUT FOR TESTING
 
 	/**
 	 * add a movie to the database
+	 * 
 	 * @param m movie object containing all the movie details
 	 */
 	public static void addMovieListing(Movie m) {
 		movieList.add(m);
-		//add to txt file
+		// add to txt file
 
 	}
-	
+
 	/**
 	 * add a cineplex to the cineplex list
-	 * @param c cinema details 
+	 * 
+	 * @param c cinema details
 	 */
 	public void addCineplex(Cineplex c) {
 		cineplexList.add(c);
 	}
-	
+
 	/**
 	 * get all movies
+	 * 
 	 * @return array of all movies
 	 */
-	public ArrayList<Movie> getMovies(){
+	public ArrayList<Movie> getMovies() {
 		return movieList;
 	}
 
-/* 	public ArrayList<Holiday> getHolidays()
-	{
-		return holidayList;
-	} */
-	
 	/**
 	 * get all cineplexes
+	 * 
 	 * @return array of all cineplexes
 	 */
-	public ArrayList<Cineplex> getCineplexes(){
+	public ArrayList<Cineplex> getCineplexes() {
 		return cineplexList;
 	}
-	
+
+	public String[] getHolidays() {
+		return holiday_list;
+	}
+
 	/**
 	 * delete a movie from a database
+	 * 
 	 * @param index ID of the movie in the file to be deleted
 	 */
 	public void deleteMovie(int index) {
 		Movie mov = movieList.get(index);
-		for (show sh:mov.getShows()) {
+		for (show sh : mov.getShows()) {
 			mov.deleteShow(this, sh);
 		}
 		movieList.remove(index);
 	}
-	
+
 	/**
 	 * read movies from the text file
 	 */
 	public void readMovies() {
 		movieIO temp = new movieIO();
 		try {
-		movieList = temp.readMovie(filename);
-		}catch(IOException e){
-			
+			movieList = temp.readMovie(filename);
+		} catch (IOException e) {
+
 		}
 	}
-	
+
 	/**
 	 * save movies into the text file
 	 */
 	public void saveMovies() {
 		movieIO temp = new movieIO();
 		try {
-		temp.saveMovie(filename, movieList);
-		}catch(IOException e){
-			
+			temp.saveMovie(filename, movieList);
+		} catch (IOException e) {
+
 		}
 	}
-	
+
 	/**
 	 * read all cineplexes from the file
 	 */
 	public void readCineplexes() {
 
-			CineplexIO temp = new CineplexIO();
-			cineplexList = temp.readCineplex();
-			
+		CineplexIO temp = new CineplexIO();
+		cineplexList = temp.readCineplex();
+
 	}
-	
+
 	/**
 	 * Save all cineplexes into the file
 	 */
@@ -122,18 +132,40 @@ public class master { // HOLIDAY IS COMMENTED OUT FOR TESTING
 
 		CineplexIO temp = new CineplexIO();
 		try {
-		temp.saveCineplex(cineplexList);
-		}
-		catch(IOException e){
-			
+			temp.saveCineplex(cineplexList);
+		} catch (IOException e) {
+
 		}
 	}
-	
+
 	/**
 	 * set a cineplex
-	 * @param c	cineplex object
+	 * 
+	 * @param c cineplex object
 	 */
 	public void setCineplexes(ArrayList<Cineplex> c) {
 		cineplexList = c;
+	}
+
+	/**
+	 * read all holidays from the file
+	 */
+	public void readHolidays() {
+		HolidayConfig temp = new HolidayConfig();
+		try {
+			holiday_list = temp.readHolidays();
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * save all holidays into the file
+	 */
+	public void saveHolidays() {
+		HolidayConfig temp = new HolidayConfig();
+		try {
+			temp.writeHoliday(holiday_list);
+		} catch (Exception e) {
+		}
 	}
 }
