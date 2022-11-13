6391c56b8db817a70b1e95c15e52ecfd777c43a4
ongkangrui <ongkangrui@gmail.com>
2022-11-13 10:49:06 +0800

    holiday implementation

--- Source Codes/src/moblima/view/ConfigureHoliday.java
+++ Source Codes/src/moblima/view/ConfigureHoliday.java
@@ -1,5 +1,6 @@
 package moblima.view;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import moblima.controller.HolidayConfig;
@@ -11,7 +12,7 @@ public class ConfigureHoliday {
      * 
      * @param args default argument
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         master m = new master();
         m.readHolidays();
@@ -36,10 +37,40 @@ public class ConfigureHoliday {
                     }
                     break;
                 case 2:
-                    System.out.print("Please enter month of holiday: ");
+                    System.out.print("Please enter month of holiday (integer): ");
                     int month = input.nextInt();
-                    System.out.print("Please enter day of holdiay: ");
+                    if (month < 1 || month > 12) {
+                        System.out.println("Invalid month!");
+                        break;
+                    }
+
+                    System.out.print("Please enter day of holdiay (integer): ");
                     int day = input.nextInt();
+                    if (Arrays.asList(1, 3, 5, 7, 8, 10, 12).contains(day)) {
+                        if (day < 1 || day > 31) {
+                            System.out.println("Invalid day!");
+                            break;
+                        }
+                    } else if (Arrays.asList(4, 6, 9, 11).contains(day)) {
+                        if (day < 1 || day > 30) {
+                            System.out.println("Invalid day!");
+                            break;
+                        }
+                    } else {
+                        if (day < 1 || day > 29) {
+                            System.out.println("Invalid day!");
+                            break;
+                        }
+                    }
+
+                    String holiday = Integer.toString(day) + '/' + Integer.toString(month);
+                    String[] newholiday_list = new String[holiday_list.length + 1];
+                    for (int i = 0; i < holiday_list.length; i++) {
+                        newholiday_list[i] = holiday_list[i];
+                    }
+                    newholiday_list[holiday_list.length] = holiday;
+                    holiday_list = newholiday_list;
+                    break;
                 case 3:
                     for (int i = 0; i < holiday_list.length; i++) {
                         System.out.printf("%d. %s", i + 1, holiday_list[i]);
@@ -47,15 +78,15 @@ public class ConfigureHoliday {
                     System.out.print("Select holiday to remove: ");
                     int remove_index = input.nextInt() - 1;
 
-                    String[] newholiday_list = new String[holiday_list.length - 1];
+                    String[] newholiday_list2 = new String[holiday_list.length - 1];
                     for (int i = 0, j = 0; i < holiday_list.length; i++) {
                         if (i == remove_index) {
                             continue;
                         }
-                        newholiday_list[j++] = holiday_list[i];
+                        newholiday_list2[j++] = holiday_list[i];
                     }
 
-                    holiday_list = newholiday_list;
+                    holiday_list = newholiday_list2;
                 case 4:
                     break;
                 default:
