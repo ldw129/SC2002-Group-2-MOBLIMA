12012d3943502b0b2ed3f994a6d22455062b6596
sgbus <limdongwan201528@gmail.com>
2022-11-13 18:46:18 +0800

    debugging cus cant run frm my side

--- Source Codes/src/moblima/controller/CineplexIO.java
+++ Source Codes/src/moblima/controller/CineplexIO.java
@@ -81,7 +81,7 @@ public class CineplexIO {
 		StringBuilder st;
 		final String SEPARATOR = ";";
 
-		for (int i = 0; i < al.size(); i++) {
+		for (int i = 0; i < al.size(); is++) {
 			c = (Cineplex) al.get(i);
 			st = new StringBuilder();
 			st.append(c.showLocation().trim());
