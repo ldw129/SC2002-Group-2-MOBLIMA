ec2df48c3d5b53f46c8168027313e802e3f14464
sgbus <limdongwan201528@gmail.com>
2022-11-13 12:47:59 +0800

    updated code for BookTickets

--- Source Codes/src/moblima/model/MovieGoerFunctions.java
+++ Source Codes/src/moblima/model/MovieGoerFunctions.java
@@ -212,22 +212,18 @@ public class MovieGoerFunctions {
             System.out.println("--- Movie Listing ---");
             for (i = 0; i < numOfMovies; i++)
                 System.out.printf(i + 1 + ". " + movies.get(i).getMovieName() + "\n");
-            
-            movie_index = sc.nextInt();
 
-            /*try {
+            try {
                 movie_index = sc.nextInt();
 
             } catch (Exception e) {
                 System.err.println("Invalid input!");
                 sc.nextLine();
-            }*/
-
-            if (movie_index > 0 && movie_index <= numOfMovies) {
-            	System.out.println("test");
-                movie = movies.get(movie_index - 1);
-                break;
+            throw e;
             }
+            
+            if (movie_index > 0 && movie_index <= numOfMovies)
+                movie = movies.get(movie_index - 1);
 
         } while (movie_index > numOfMovies);
         
@@ -407,99 +403,105 @@ public class MovieGoerFunctions {
         m = selectMovie(moviesAvailableForBooking, moviesAvailableForBooking.size());        
 
         ArrayList<show> showsOfSelectedMovie = m.getShows();
+        System.out.println(showsOfSelectedMovie);
+        System.out.println(showsOfSelectedMovie.size());
         holidayList = holIO.readHolidays();
         
-        for (int i = 0; i < showsOfSelectedMovie.size(); i++) {
-        	publicHols = false;
-        	show = showsOfSelectedMovie.get(i);
-        	showtime = show.getDateTime().split(" ")[0];
-        	
-        	for (int h = 0; h < holidayList.length; h++) {
-        		if (holidayList[h].equals(showtime)) {
-        			publicHols = true;
-        			break;
-        		}
-        	}
-        	
-			cinemaClass = Master.getCineplexes().get(show.getCineplexID()).getCinema().get(show.getScreenNum()).getCinemaClass();
-			// MovieTicket price = new MovieTicket(show.get3D(), movieDetails, ageCat, publicHols);
-			// MovieTicket price = new MovieTicket(String typeofmovie, String cinemaclass, int age, int date);
-			System.out.println(" ");
-			System.out.printf("\nShow %d:\n", i+1);
-			System.out.println("Date & Time: \n" + show.getDateTime());
-			System.out.printf("Cineplex ID: %d\n", show.getCineplexID()+1);
-			System.out.printf("Cinema ID: %d\n", show.getScreenNum()+1);
-			System.out.printf("Cinema Class: %s\n", cinemaClass);
-			System.out.printf("3D Movie? %s\n", show.get3D());
-			// System.out.printf("Ticket Price: S$%s (Inclusive of GST)\n", price.getPrice());
-			System.out.println("----------------------------------------------");
-        }
-        
-        try {
-        	System.out.println("Enter show index: ");
-            show_index = sc.nextInt() - 1;
-            
-            show = showsOfSelectedMovie.get(show_index);
-            show.printSeats();
-            
-            System.out.println("Enter the total number of seats: ");
-            numSeats = sc.nextInt();
-            
-            System.out.println("Enter the first seat (ie. A1: Row 1 Seat 1; B2: Row 2 Seat 2): "); // seat ID formatting to be confirmed
-            firstSeat = sc.next();
-        } catch (Exception e) {
-            System.err.println("Invalid input!");
-            sc.nextLine();
-        }
-        
-        inputValidation = true;
-        
-        while (inputValidation) {
-    		System.out.println("Confirm booking? Y/N");
-            bookingConfirmation = sc.next();
-            
-        	if (bookingConfirmation.equals("n" ) || bookingConfirmation.equals("N" )) {
-            	System.out.println("Confirm cancellation? Y/N");
-            	cancel = sc.next();
-            	if (cancel.equals("y") || cancel.equals("Y")) {
-            		System.out.println("Booking cancelled!");
-            		break;
+        if (showsOfSelectedMovie.size() > 0) {
+        	for (int i = 0; i < showsOfSelectedMovie.size(); i++) {
+            	publicHols = false;
+            	show = showsOfSelectedMovie.get(i);
+            	showtime = show.getDateTime().split(" ")[0];
+            	
+            	for (int h = 0; h < holidayList.length; h++) {
+            		if (holidayList[h].equals(showtime)) {
+            			publicHols = true;
+            			break;
+            		}
             	}
-            	else
-            		System.out.println("Booking continued");
-        			continue;
+            	
+    			cinemaClass = Master.getCineplexes().get(show.getCineplexID()).getCinema().get(show.getScreenNum()).getCinemaClass();
+    			// MovieTicket price = new MovieTicket(show.get3D(), movieDetails, ageCat, publicHols);
+    			// MovieTicket price = new MovieTicket(String typeofmovie, String cinemaclass, int age, int date);
+    			System.out.println(" ");
+    			System.out.printf("\nShow %d:\n", i+1);
+    			System.out.println("Date & Time: \n" + show.getDateTime());
+    			System.out.printf("Cineplex ID: %d\n", show.getCineplexID()+1);
+    			System.out.printf("Cinema ID: %d\n", show.getScreenNum()+1);
+    			System.out.printf("Cinema Class: %s\n", cinemaClass);
+    			System.out.printf("3D Movie? %s\n", show.get3D());
+    			// System.out.printf("Ticket Price: S$%s (Inclusive of GST)\n", price.getPrice());
+    			System.out.println("----------------------------------------------");
             }
-            else if (bookingConfirmation.equals("y" ) || bookingConfirmation.equals("Y" )) {
-            	BookingInfo b = new BookingInfo(show.getCineplexID());
-            	transaction_id = b.getTID();
-            	ch = firstSeat.charAt(0);
-	    		firstSeatNum = Character.getNumericValue(firstSeat.charAt(1))-1;
-	        	row = ch - 'a';
-	        	seatAssigned = false;
-	        	
-	        	for (int i = 0; i < numSeats;i++) {
-	        		if (show.checkSeat(row, firstSeatNum + i)) // seat has already been assigned
-	        			seatAssigned = true;
-	        	}
-	        	
-	        	if (!seatAssigned) { // seatAssigned = false
-	        		try {
-	        			mg.assignFinalSeatsbyMovie(m, show_index, cust_name, cust_id, cust_email, cust_mobile, transaction_id, numSeats, firstSeat);
-	        		} catch (Exception e) {
-	        			e.printStackTrace();
-	        		}
-	        		
-	        	}
-	        	
-	        	System.out.println("Booking ID: " + transaction_id);
-            	System.out.println("Movie tickets successfully booked!");
-            	inputValidation = false;
+            
+            try {
+            	System.out.println("Enter show index: ");
+                show_index = sc.nextInt() - 1;
+                
+                show = showsOfSelectedMovie.get(show_index);
+                show.printSeats();
+                
+                System.out.println("Enter the total number of seats: ");
+                numSeats = sc.nextInt();
+                
+                System.out.println("Enter the first seat (ie. A1: Row 1 Seat 1; B2: Row 2 Seat 2): "); // seat ID formatting to be confirmed
+                firstSeat = sc.next();
+            } catch (Exception e) {
+                System.err.println("Invalid input!");
+                sc.nextLine();
             }
-            else {
-            	System.err.println("Invalid input!");
-        		continue;
-            }   	
+            
+            inputValidation = true;
+            
+            while (inputValidation) {
+        		System.out.println("Confirm booking? Y/N");
+                bookingConfirmation = sc.next();
+                
+            	if (bookingConfirmation.equals("n" ) || bookingConfirmation.equals("N" )) {
+                	System.out.println("Confirm cancellation? Y/N");
+                	cancel = sc.next();
+                	if (cancel.equals("y") || cancel.equals("Y")) {
+                		System.out.println("Booking cancelled!");
+                		break;
+                	}
+                	else
+                		System.out.println("Booking continued");
+            			continue;
+                }
+                else if (bookingConfirmation.equals("y" ) || bookingConfirmation.equals("Y" )) {
+                	BookingInfo b = new BookingInfo(show.getCineplexID());
+                	transaction_id = b.getTID();
+                	ch = firstSeat.charAt(0);
+    	    		firstSeatNum = Character.getNumericValue(firstSeat.charAt(1))-1;
+    	        	row = ch - 'a';
+    	        	seatAssigned = false;
+    	        	
+    	        	for (int i = 0; i < numSeats;i++) {
+    	        		if (show.checkSeat(row, firstSeatNum + i)) // seat has already been assigned
+    	        			seatAssigned = true;
+    	        	}
+    	        	
+    	        	if (!seatAssigned) { // seatAssigned = false
+    	        		try {
+    	        			mg.assignFinalSeatsbyMovie(m, show_index, cust_name, cust_id, cust_email, cust_mobile, transaction_id, numSeats, firstSeat);
+    	        		} catch (Exception e) {
+    	        			e.printStackTrace();
+    	        		}
+    	        		
+    	        	}
+    	        	
+    	        	System.out.println("Booking ID: " + transaction_id);
+                	System.out.println("Movie tickets successfully booked!");
+                	inputValidation = false;
+                }
+                else {
+                	System.err.println("Invalid input!");
+            		continue;
+                }   	
+            }
         }
+        
+        
         
         return;
     }
